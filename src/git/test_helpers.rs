568722f79853ae3a521d464cb6b6c0140d63d529
xpadev <xpadev@gmail.com>
2026-05-24 02:01:50 +0900

    fix(phase12): remove unused init_repo_at from test_helpers

--- src/git/test_helpers.rs
+++ src/git/test_helpers.rs
@@ -25,26 +25,3 @@ pub fn init_repo(tmp: &tempfile::TempDir) -> Utf8PathBuf {
     }
     dir
 }
-
-/// Like `init_repo` but initialises directly in `dir` (already a `Utf8Path`).
-pub fn init_repo_at(dir: &camino::Utf8Path) {
-    for cmd in [
-        vec!["init"],
-        vec!["config", "user.email", "test@test.com"],
-        vec!["config", "user.name", "Test"],
-    ] {
-        std::process::Command::new("git")
-            .args(&cmd)
-            .current_dir(dir)
-            .output()
-            .unwrap();
-    }
-    std::fs::write(dir.join("README"), b"init").unwrap();
-    for cmd in [vec!["add", "."], vec!["commit", "-m", "init"]] {
-        std::process::Command::new("git")
-            .args(&cmd)
-            .current_dir(dir)
-            .output()
-            .unwrap();
-    }
-}
